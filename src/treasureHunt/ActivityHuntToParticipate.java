3afe35b7ea071dfca7246e6b4a01a2a0c24ce905
Duplouy Olivier <olivier.duplouy@etud.univ-montp2.fr>
2013-12-27 17:59:01 +0100

    Remise à plat en cours...

--- src/treasureHunt/ActivityHuntToParticipate.java
+++ src/treasureHunt/ActivityHuntToParticipate.java
@@ -3,16 +3,22 @@ package treasureHunt;
 import com.example.treasurehunt2.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class ActivityHuntToParticipate extends Activity {
+	
+	EditText hunt_name;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hunt_to_participate);
-		//TODO Mise en place de la BDD ou pas
+		hunt_name=(EditText)findViewById(R.id.name_hunt_choosen);
 	}
 
 	@Override
@@ -20,5 +26,24 @@ public class ActivityHuntToParticipate extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.treasure_hunt, menu);
 		return true;
+	}
+	
+	public void launch(View v){
+		if (hunt_name.getText().length()!=0){
+			//TODO import des données vers la BD interne
+			
+		}else{
+			AlertDialog.Builder localBuilder = new AlertDialog.Builder(this);
+			localBuilder
+			.setMessage("Pour participer il faut rentrer un de chasse au trésor.")
+			.setCancelable(false)
+			.setNeutralButton("Ok",
+					new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+				}
+			}
+					);
+			localBuilder.create().show();
+		}
 	}
 }
--- src/treasureHunt/ActivityParticipation.java
+++ src/treasureHunt/ActivityParticipation.java
@@ -5,7 +5,6 @@ import com.example.treasurehunt2.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 public class ActivityParticipation extends Activity {
 	
@@ -21,9 +20,5 @@ public class ActivityParticipation extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.treasure_hunt, menu);
 		return true;
-	}
-	
-	public void launch(View v){
-		
 	}
 }
